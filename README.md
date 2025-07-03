8f0e7c85f1df6c3bdfe7f08971dc4f6e4525be31
eidolon1302 <gigantician13@gmail.com>
2025-07-03 13:02:27 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# My Example CLI
+
+This is a simple example CLI to demonstrate how to build and distribute a Python command-line tool.
+
+## Installation
+
+```bash
+pip install git+https://github.com/your-username/my-example-cli.git
+```
+
+## Usage
+
+```bash
+my-cli YourName --times 3
+```
---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="my-example-cli",
+    version="0.1.0",
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'my-cli=my_cli:main',
+        ],
+    },
+    author="Your Name",
+    author_email="your.email@example.com",
+    description="A simple example CLI.",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/your-username/my-example-cli",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
